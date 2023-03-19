fa7e672665228abecaac07ae9d0906c03516af12
Alejandra <estefany.crug@gmail.com>
2023-03-19 15:39:50 -0600

    Commit - ActivityView

--- app/src/main/java/com/example/ExaIIP/ActivityView.java
+++ app/src/main/java/com/example/ExaIIP/ActivityView.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ActivityView extends AppCompatActivity {
 
+    ImageView picture;
     Button editar, eliminar, contactos, mapa;
-
+    EditText nombre, telefono, lat, lon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,11 @@ public class ActivityView extends AppCompatActivity {
         contactos = (Button) findViewById(R.id.btn_contacto);
         editar = (Button) findViewById(R.id.btn_editar);
         mapa = (Button) findViewById(R.id.btn_mapa);
+        picture = (ImageView) findViewById(R.id.imageView);
+        nombre = (EditText) findViewById(R.id.txtNombre);
+        telefono = (EditText) findViewById(R.id.txtTelefono);
+        lat = (EditText) findViewById(R.id.txtLatitud);
+        lon = (EditText) findViewById(R.id.txtLongitud);
 
         contactos.setOnClickListener(new View.OnClickListener() {
             @Override
